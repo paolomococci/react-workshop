592bfc52f935dd78d2059b19968fee1c07e0ae19
paolomococci <paolomococci@gmail.com>
2021-10-15 19:09:48 +0200

    some changes to the project example-clean

--- example-clean/src/App.tsx
+++ example-clean/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>
-          example clean
+          example clean project
         </p>
       </header>
     </div>
