c38f8f26763f0e0d1faac1afdc8004e290a257e0
paolomococci <paolomococci@gmail.com>
2021-10-21 08:09:06 +0200

    I added the project notes-on-things-to-do

---
+++ notes-on-things-to-do/src/App.tsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <h3>
+          Notes of Things To Do
+        </h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
