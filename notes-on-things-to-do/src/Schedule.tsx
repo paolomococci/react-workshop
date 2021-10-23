68019a1929bdf561a846f9361fc6ff7391bbfc3d
paolomococci <paolomococci@gmail.com>
2021-10-23 10:34:51 +0200

    I added the Schedule and Task components

---
+++ notes-on-things-to-do/src/Schedule.tsx
@@ -0,0 +1,29 @@
+import { FC, useEffect } from 'react';
+import Task from './Task';
+
+export type Pointer = {
+    id: number
+    task: string
+}
+
+interface Props {
+    pointers: Pointer[]
+    handleDelete: any
+}
+
+const Schedule: FC<Props> = ({ pointers, handleDelete }) => {
+
+    useEffect(() => {
+        console.log('Rendering <Schedule/>')
+    })
+
+    return (
+        <ul>
+            {pointers.map((pointer: Pointer) => (
+                <Task key={pointer.id} id={pointer.id} task={pointer.task} handleDelete={handleDelete}/>
+            ))}
+        </ul>
+    )
+}
+
+export default Schedule
---
+++ notes-on-things-to-do/src/Task.tsx
@@ -0,0 +1,20 @@
+import { FC, useEffect, memo } from 'react';
+
+interface Props {
+    id: number
+    task: string
+    handleDelete: any
+}
+
+const Task: FC<Props> = ({ id, task, handleDelete }) => {
+
+    useEffect(() => {
+        console.log('Rendering <Task/>', task)
+    })
+    
+    return (
+        <li>{task} <button onClick={() => handleDelete(id)}>Del</button></li>
+    )
+}
+
+export default memo(Task)
