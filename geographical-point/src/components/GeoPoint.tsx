57b3660d4fdb30ac66c52cf7caf5de51bc228638
paolomococci <paolomococci@gmail.com>
2022-03-16 20:07:01 +0100

    edited componet GeoPoint to project geographical-point

--- geographical-point/src/components/GeoPoint.tsx
+++ geographical-point/src/components/GeoPoint.tsx
@@ -2,3 +2,13 @@ import { useState, useEffect } from "react";
 
 const [latitude, setLatitude] = useState<number | null>(null)
 const [longitude, setLongitude] = useState<number | null>(null)
+
+useEffect (() => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(handleSuccess)
+  }
+}, [])
+
+function handleSuccess(handleSuccess: any) {
+  throw new Error("Function not implemented.");
+}
