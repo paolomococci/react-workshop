578cbeb7d69d1aaa28e08c1bc91150720f2fad5b
paolomococci <paolomococci@gmail.com>
2022-03-16 22:31:46 +0100

    edited file GeoLocate.tsx to project geographical-point

--- geographical-point/src/components/GeoLocate.tsx
+++ geographical-point/src/components/GeoLocate.tsx
@@ -1,7 +1,21 @@
-import { type } from "os";
 import { FC } from "react";
 
 type Props = {
   latitude: number
   longitude: number
 }
+
+const GeoLocate: FC<Props> = ({
+  latitude,
+  longitude
+}) => (
+  <div>
+    <h1>
+      Geo Locate
+    </h1>
+    <div>
+      <output>latitude: {latitude}</output>
+      <output>longitude: {longitude}</output>
+    </div>
+  </div>
+)
