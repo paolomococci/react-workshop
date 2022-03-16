0260729e719630a34d156482f4477e418ed49aa2
paolomococci <paolomococci@gmail.com>
2022-03-16 23:02:46 +0100

    edited file App.tsx to project geographical-point

--- geographical-point/src/App.tsx
+++ geographical-point/src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GeoLocateContainer from './components/GeoLocateContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <GeoLocateContainer />
       </header>
     </div>
   );
