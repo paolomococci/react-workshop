88cab5a2ea0aa0294870003494e52290d0f34670
paolomococci <paolomococci@gmail.com>
2022-03-16 22:51:03 +0100

    edited file GeoLocateContainer.tsx to project geographical-point

--- geographical-point/src/components/GeoLocateContainer.tsx
+++ geographical-point/src/components/GeoLocateContainer.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 
 const GeoLocateContainer = () => {
-
+  const [latitude, setLatitude] = useState<number | null>(null)
+const [longitude, setLongitude] = useState<number | null>(null)
 }
