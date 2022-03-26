8313b24dfa96b32fadd3a6a5c392bbdfb6b3404e
paolomococci <paolomococci@gmail.com>
2022-03-26 18:52:09 +0100

    I modify the index.tsx file belonging to the diving-hoc project

--- diving-hoc/src/components/SampleComponent/index.tsx
+++ diving-hoc/src/components/SampleComponent/index.tsx
@@ -1,1 +1,10 @@
 import { FC } from "react";
+import withInnerWidth from "./withInnerWidth";
+
+type Props = {
+  innerWidth: number
+}
+
+const SampleComponent: FC<Props> = ({ innerWidth }) => <div>innerWidth: {innerWidth}</div>
+
+export default withInnerWidth(SampleComponent)
