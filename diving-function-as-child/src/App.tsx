87ab2d153e709a7e6313e0a40e905efcddf483cb
paolomococci <paolomococci@gmail.com>
2022-03-27 23:45:05 +0200

    edited file App.tsx belonging to project diving-function-as-child

--- diving-function-as-child/src/App.tsx
+++ diving-function-as-child/src/App.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Name from './components/Name'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+      <Name>
+        {(name: string) => <div>Hello, {name}!</div>}
+      </Name>
     </div>
   );
 }
