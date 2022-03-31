70b50b7555c520afc628b23aad69bf2e540b8758
paolomococci <paolomococci@gmail.com>
2022-03-31 09:47:39 +0200

    added file auth.ts belonging to project user-registration-system-graphql-api

---
+++ user-registration-system-graphql-api/backend/src/lib/auth.ts
@@ -0,0 +1,4 @@
+import { AuthenticationError } from 'apollo-server'
+import { encrypt, isPasswordMatch } from '@contentpi/lib'
+import { IUser, IModels, IAuthPayload } from '../types'
+import { createToken } from './jwt'
