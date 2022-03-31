37abe6c49432d52d4b4eac322a1156d54e2395b6
paolomococci <paolomococci@gmail.com>
2022-03-31 09:55:09 +0200

    edited file index.ts belonging to project user-registration-system-graphql-api

--- user-registration-system-graphql-api/backend/config/index.ts
+++ user-registration-system-graphql-api/backend/config/index.ts
@@ -1,2 +1,26 @@
 import dotenv from 'dotenv'
 import config from './config.json'
+
+dotenv.config()
+
+type Db = {}
+
+type Security = {}
+
+type Server = {}
+
+const {
+  DB_DIALECT = '',
+  DB_PORT = '',
+  DB_HOST = '',
+  DB_DATABASE = '',
+  DB_USERNAME = '',
+  DB_PASSWORD = '',
+} = process.env
+
+const db: Db = {}
+
+const {
+  security, server
+} = config
+
