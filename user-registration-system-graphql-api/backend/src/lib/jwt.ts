ff6a1e89f5f1018ae51d2368fcf75cc1862d1487
paolomococci <paolomococci@gmail.com>
2022-04-03 00:29:13 +0200

    edited file jwt.ts belonging to project user-registration-system-graphql-api

--- user-registration-system-graphql-api/backend/src/lib/jwt.ts
+++ user-registration-system-graphql-api/backend/src/lib/jwt.ts
@@ -2,10 +2,6 @@ import jwt from 'jsonwebtoken'
 import { encrypt, setBase64, getBase64 } from '@contentpi/lib'
 import { $security } from '../../config'
 import { IUser } from '../types'
-import jwt from 'jsonwebtoken'
-import { encrypt, setBase64, getBase64 } from '@contentpi/lib'
-import { $security } from '../../config'
-import { IUser } from '../types'
 
 const { secretKey } = $security
 
@@ -35,5 +31,17 @@ export async function getUserData(accessToken: string): Promise<any> {
 }
 
 export const createToken = async (user: IUser): Promise<string[]> => {
-
+  const { id, username, password, email, privilege, active } = user
+  const token = setBase64(`${encrypt($security.secretKey)}${password}`)
+  const userData = {}
+  const _createToken = jwt.sign(
+    {
+      data: setBase64(userData)
+    },
+    $security.secretKey,
+    {
+      expiresIn: $security.expiresIn
+    }
+  )
+  return Promise.all([_createToken])
 }
