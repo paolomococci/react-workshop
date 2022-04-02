a72bea2a49e2d4cf6144a1852f3ed87aa889b4a4
paolomococci <paolomococci@gmail.com>
2022-04-03 00:52:44 +0200

    edited file User.ts belonging to project user-registration-system-graphql-api

--- user-registration-system-graphql-api/backend/src/models/User.ts
+++ user-registration-system-graphql-api/backend/src/models/User.ts
@@ -1,2 +1,28 @@
 import { encrypt } from '@contentpi/lib'
 import { IUser, IDataTypes } from '../types'
+
+export default (sequelize: any, DataTypes: IDataTypes): IUser => {
+  const User = sequelize.define(
+    'User', {
+      id: {
+        
+      },
+      username: {
+
+      },
+      password: {
+
+      },
+      email: {
+
+      },
+      privilege: {
+
+      },
+      active: {
+
+      }
+    },
+
+  )
+}
