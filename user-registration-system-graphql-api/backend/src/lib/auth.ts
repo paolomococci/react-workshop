e2c8838ccd5a3a3fc4a1fb4149afea1bffd6adbe
paolomococci <paolomococci@gmail.com>
2022-04-02 23:32:12 +0200

    edited file auth.ts belonging to project user-registration-system-graphql-api

--- user-registration-system-graphql-api/backend/src/lib/auth.ts
+++ user-registration-system-graphql-api/backend/src/lib/auth.ts
@@ -11,6 +11,13 @@ export const getUserBy = async (
   where: any, 
   models: IModels
 ): Promise<IUser> => {
+}
+
+export const doLogin = async (
+  email: string, 
+  password: string, 
+  models: IModels
+): Promise<IAuthPayload> => {
   const user = await getUserBy({ email }, models)
   if (!user) {}
   const passwordMatch = isPasswordMatch(encrypt(password), user.password)
@@ -20,9 +27,3 @@ export const getUserBy = async (
   const [token] = await createToken(user)
   return { token }
 }
-
-export const doLogin = async (
-  email: string, 
-  password: string, 
-  models: IModels
-): Promise<IAuthPayload> => {}
