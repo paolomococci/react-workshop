3413293c5aed106566a5e582911f902bc45f9914
paolomococci <paolomococci@gmail.com>
2022-04-02 22:23:37 +0200

    edited file user.ts belonging to project user-registration-system-graphql-api

--- user-registration-system-graphql-api/backend/src/graphql/resolvers/user.ts
+++ user-registration-system-graphql-api/backend/src/graphql/resolvers/user.ts
@@ -3,6 +3,17 @@ import { doLogin, getUserBy } from '../../lib/auth'
 import { IUser, ICreateUserInput, IModels, ILoginInput, IAuthPayload } from '../../types'
 
 export default {
-  Query: {},
+  Query: {
+    getUsers: (
+      _: any, args: any, { models }: { models: IModels }
+    ): IUser[] => models.User.findAll(),
+    getUserData: async (
+      _: any,
+      { at }: { at: string },
+      { models }: { models: IModels }
+    ): Promise<any> => {
+      
+    }
+  },
   Mutation: {}
 }
