79c665c771f466e5182a33fa1970ad8b29e0e767
paolomococci <paolomococci@gmail.com>
2022-04-03 00:06:30 +0200

    edited file jwt.ts belonging to project user-registration-system-graphql-api

--- user-registration-system-graphql-api/backend/src/lib/jwt.ts
+++ user-registration-system-graphql-api/backend/src/lib/jwt.ts
@@ -5,5 +5,18 @@ import { IUser } from '../types'
 import jwt from 'jsonwebtoken'
 import { encrypt, setBase64, getBase64 } from '@contentpi/lib'
 import { $security } from '../../config'
+import { IUser } from '../types'
 
 const { secretKey } = $security
+
+export function jwtVerify(accessToken: string, cb: any): void {
+  
+}
+
+export async function getUserData(accessToken: string): Promise<any> {
+
+}
+
+export const createToken = async (user: IUser): Promise<string[]> => {
+
+}
