0450285ab268ed8c522a73f0d02e3d89a5738c77
paolomococci <paolomococci@gmail.com>
2022-04-02 22:06:08 +0200

    edited file user.ts belonging to project user-registration-system-graphql-api

---
+++ user-registration-system-graphql-api/backend/src/graphql/resolvers/user.ts
@@ -0,0 +1,8 @@
+import { getUserData } from '../../lib/jwt'
+import { doLogin, getUserBy } from '../../lib/auth'
+import { IUser, ICreateUserInput, IModels, ILoginInput, IAuthPayload } from '../../types'
+
+export default {
+  Query: {},
+  Mutation: {}
+}
