3bb543ff99df8d0548d3ee95daf97baee3f7fb9d
paolomococci <paolomococci@gmail.com>
2022-04-02 22:10:49 +0200

    edited file index.ts belonging to project user-registration-system-graphql-api

--- user-registration-system-graphql-api/backend/src/index.ts
+++ user-registration-system-graphql-api/backend/src/index.ts
@@ -1,3 +1,19 @@
 import { ApolloServer } from 'apollo-server'
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import { $server } from '../config'
+import models from './models'
+import resolvers from './graphql/resolvers'
+import typeDefs from './graphql/types'
+
+const alter = true
+const force = false
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+const apolloServer = new ApolloServer({ schema, context: { models }})
+
+models.sequelize.sync({ alter, force }).then(() => {
+  apolloServer.listen($server.port).then(({ url }) => { 
+    console.log(`Running on ${url}`)
+   })
+})
