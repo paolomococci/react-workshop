b7230442effd6a96850a6f2dcdb5d10b50ae97c7
paolomococci <paolomococci@gmail.com>
2022-04-03 01:28:49 +0200

    edited file interfaces.ts belonging to project user-registration-system-graphql-api

--- user-registration-system-graphql-api/backend/src/types/interfaces.ts
+++ user-registration-system-graphql-api/backend/src/types/interfaces.ts
@@ -1,1 +1,13 @@
 import { User, Sequelize } from './types'
+
+export interface IDataTypes {}
+
+export interface IUser extends User, Sequelize {}
+
+export interface ICreateUserInput extends User {}
+
+export interface ILoginInput {}
+
+export interface IAuthPayload {}
+
+export interface IModels {}
