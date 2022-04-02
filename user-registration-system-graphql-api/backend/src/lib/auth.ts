77b76be1a061211231ab3a41f5991032b7fee5d3
paolomococci <paolomococci@gmail.com>
2022-04-02 23:13:26 +0200

    edited file auth.ts belonging to project user-registration-system-graphql-api

--- user-registration-system-graphql-api/backend/src/lib/auth.ts
+++ user-registration-system-graphql-api/backend/src/lib/auth.ts
@@ -4,3 +4,9 @@ import { IUser, IModels, IAuthPayload } from '../types'
 import { createToken } from './jwt'
 import { AuthenticationError } from 'apollo-server'
 import { encrypt, isPasswordMatch } from '@contentpi/lib'
+import { IUser, IModels, IAuthPayload } from '../types'
+import { createToken } from './jwt'
+
+export const getUserBy = async (): Promise<IUser> => {}
+
+export const doLogin = async (): Promise<IAuthPayload> => {}
