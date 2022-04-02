39cb5c8d05f2d4de59f536d4bb649bd001df67c8
paolomococci <paolomococci@gmail.com>
2022-04-03 00:40:44 +0200

    edited file index.ts belonging to project user-registration-system-graphql-api

--- user-registration-system-graphql-api/backend/src/models/index.ts
+++ user-registration-system-graphql-api/backend/src/models/index.ts
@@ -1,1 +1,12 @@
 import { Sequelize } from 'sequelize'
+import { $db } from '../../config'
+import { IModels } from '../types'
+
+const { 
+  dialect, 
+  port, 
+  host, 
+  database, 
+  username, 
+  password 
+} = $db
