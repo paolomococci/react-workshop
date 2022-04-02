40bd35b0be7b8ad8a27d84156f79cbfbb668aaa4
paolomococci <paolomococci@gmail.com>
2022-04-02 21:51:10 +0200

    edited file jwt.ts belonging to project user-registration-system-graphql-api

--- user-registration-system-graphql-api/backend/src/lib/jwt.ts
+++ user-registration-system-graphql-api/backend/src/lib/jwt.ts
@@ -4,3 +4,6 @@ import { $security } from '../../config'
 import { IUser } from '../types'
 import jwt from 'jsonwebtoken'
 import { encrypt, setBase64, getBase64 } from '@contentpi/lib'
+import { $security } from '../../config'
+
+const { secretKey } = $security
