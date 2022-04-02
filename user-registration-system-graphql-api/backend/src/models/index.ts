c6cd565b99696225556d4820f8c22927a856aba4
paolomococci <paolomococci@gmail.com>
2022-04-03 00:45:52 +0200

    edited file index.ts belonging to project user-registration-system-graphql-api

--- user-registration-system-graphql-api/backend/src/models/index.ts
+++ user-registration-system-graphql-api/backend/src/models/index.ts
@@ -15,6 +15,9 @@ const uri = `${dialect}://${username}:${password}@${host}:${port}/${database}`
 
 const sequelize = new Sequelize(uri)
 
-const models: IModels = {}
+const models: IModels = {
+  User: require('./User').default(sequelize, Sequelize), 
+  sequelize
+}
 
 export default models
