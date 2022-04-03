d4fc6e0409a6ebb8f52268b3432cd366feb195e8
paolomococci <paolomococci@gmail.com>
2022-04-03 15:55:45 +0200

    edited file index.ts belonging to project user-registration-system-graphql-api

--- user-registration-system-graphql-api/backend/src/index.ts
+++ user-registration-system-graphql-api/backend/src/index.ts
@@ -7,7 +7,7 @@ import typeDefs from './graphql/types'
 
 const alter = true
 
-// If you intend to make a clean sweep of the data just change the value of the constant "force" to true
+// if you intend to make a clean sweep of the data just change the value of the constant "force" to true
 const force = false
 
 const schema = makeExecutableSchema({ typeDefs, resolvers })
@@ -16,6 +16,6 @@ const apolloServer = new ApolloServer({ schema, context: { models }})
 
 models.sequelize.sync({ alter, force }).then(() => {
   apolloServer.listen($server.port).then(({ url }) => { 
-    console.log(`Running on ${url}`)
+    console.log(`this application responds to ${url}`)
    })
 })
