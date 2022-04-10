f7109b9f30e4115adee74f8697bf3c8a79eee2a5
paolomococci <paolomococci@gmail.com>
2022-04-10 20:30:25 +0200

    edited file jwt.ts belonging to project raw-api

--- user-registration-system/raw-api/src/lib/jwt.ts
+++ user-registration-system/raw-api/src/lib/jwt.ts
@@ -2,3 +2,5 @@ import jwt from 'jsonwebtoken'
 import { encrypt, setBase64, getBase64 } from '@contentpi/lib'
 import { $security } from '../../config'
 import { IUser } from '../types'
+
+const { secretKey } = $security
