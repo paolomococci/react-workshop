10564b8cb10a0c68af86221ae6258756a93cca9d
paolomococci <paolomococci@gmail.com>
2022-04-10 21:12:47 +0200

    edited file auth.ts belonging to project raw-api

--- user-registration-system/raw-api/src/lib/auth.ts
+++ user-registration-system/raw-api/src/lib/auth.ts
@@ -2,3 +2,8 @@ import { AuthenticationError } from 'apollo-server'
 import { encrypt, isPasswordMatch } from '@contentpi/lib'
 import { IUser, IModels, IAuthPayload } from '../types'
 import { createToken } from './jwt'
+
+export const getUserBy = async (
+  where: any,
+  models: IModels
+): Promise<IUser> => {}
