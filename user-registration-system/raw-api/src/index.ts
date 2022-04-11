881a340849075eaf27efb28e6d9c6fd56810d8e1
paolomococci <paolomococci@gmail.com>
2022-04-11 08:24:38 +0200

    edited file index.ts belonging to project raw-api

--- user-registration-system/raw-api/src/index.ts
+++ user-registration-system/raw-api/src/index.ts
@@ -4,3 +4,10 @@ import models from './models'
 import resolvers from './graphql/resolvers'
 import typeDefs from './graphql/types'
 import { $server } from '../config'
+
+const schema = makeExecutableSchema(
+  {
+    typeDefs,
+    resolvers
+  }
+)
