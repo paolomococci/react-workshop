7fab5cc3577eebb1444c891f23f5d477a6a8b306
paolomococci <paolomococci@gmail.com>
2022-04-13 16:51:43 +0200

    edited file src/types/interfaces.ts belonging to project legacy-api

---
+++ user-registration-system/legacy-api/src/types/interfaces.ts
@@ -0,0 +1,40 @@
+import { User, Sequelize } from './types'
+
+/* User */
+export interface IUser extends User, Sequelize {
+  id: string
+  token?: string
+  createdAt?: Date
+  updatedAt?: date
+}
+
+export interface ICreateUserInput extends User {
+
+}
+
+export interface ILoginInput {
+  email: string
+  password: string
+}
+
+export interface IAuthPayload {
+  token: string
+}
+
+/* Sequelize */
+export interface IDataTypes {
+  UUID: string
+  UUIDV4(): string
+  STRING: string
+  BOOLEAN: boolean
+  TEXT: string
+  INTEGER: number
+  DATE: string
+  FLOAT: number
+}
+
+/* Models */
+export interface IModels {
+  User: any
+  sequelize: any
+}
