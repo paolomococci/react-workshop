d3d8d82862a424a4fb64ab27c419b39e2a7981bf
paolomococci <paolomococci@gmail.com>
2022-04-22 10:11:19 +0200

    edited file src/server.ts belonging to project users-ui

---
+++ users-admin/users-ui/src/server.ts
@@ -0,0 +1,7 @@
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import session from 'express-session'
+import express, {
+  NextFunction, Request, Response
+} from 'express'
