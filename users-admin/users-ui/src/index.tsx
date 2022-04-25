8057d4ac167b335d473e747c35e55c7ed51a6cb3
paolomococci <paolomococci@gmail.com>
2022-04-25 22:31:59 +0200

    edited file src/index.tsx belonging to project users-ui

--- users-admin/users-ui/src/index.tsx
+++ users-admin/users-ui/src/index.tsx
@@ -4,3 +4,18 @@ import {
   ApolloClient,
   InMemoryCache
 } from '@apollo/client'
+
+import AppRoutes from './AppRoutes'
+import config from './config'
+
+const client = new ApolloClient(
+  {
+    uri: config.apiUrl,
+    cache: new InMemoryCache()
+  }
+)
+
+ReactDOM.render(
+  <ApolloProvider client={client}><AppRoutes /></ApolloProvider>,
+  document.querySelector('#root')
+)
