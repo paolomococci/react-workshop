8b2617fe8588e7054c89da97ce4bea6e84bee17b
paolomococci <paolomococci@gmail.com>
2022-04-25 08:11:57 +0200

    edited file src/graphql/user/getUserData.query.ts belonging to project users-ui

---
+++ users-admin/users-ui/src/graphql/user/getUserData.query.ts
@@ -0,0 +1,15 @@
+import {
+  gql
+} from '@apollo/client';
+
+export default gql`
+  query getUserData($at: String!) {
+    getUserData(at: $at) {
+      id
+      email
+      username
+      privilege
+      active
+    }
+  }
+`
