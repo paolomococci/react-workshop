95bfc20c077b050292eddae089279a2799b108fc
paolomococci <paolomococci@gmail.com>
2022-04-25 22:32:34 +0200

    edited file src/server.ts belonging to project users-ui

--- users-admin/users-ui/src/server.ts
+++ users-admin/users-ui/src/server.ts
@@ -7,3 +7,108 @@ import express, {
   Request,
   Response
 } from 'express'
+
+import {
+  isConnected
+} from './lib/middlewares/user'
+
+import config from './config'
+
+const app = express()
+const port = process.env.NODE_PORT || 3000
+const DIST_DIR = path.join(__dirname, '../dist')
+const HTML_FILE = path.join(DIST_DIR, 'index.html')
+
+app.use(express.static(DIST_DIR))
+
+app.use(
+  session(
+    {
+      resave: false,
+      saveUninitialized: true,
+      secret: config.security.secretKey
+    }
+  )
+)
+
+app.use(
+  express.json()
+)
+
+app.use(
+  express.urlencoded(
+    {
+      extended: true
+    }
+  )
+)
+
+app.use(
+  cookieParser(config.security.secretKey)
+)
+
+app.use(
+  cors(
+    {
+      credentials: true,
+      origin: true
+    }
+  )
+)
+
+app.get(
+  '/dashboard',
+  isConnected(
+    true,
+    [
+      'admin',
+      'normal'
+    ],
+    `/login?redirectTo=/dashboard`
+  ),
+  (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    next()
+  }
+)
+
+app.get(
+  '/login',
+  isConnected(false),
+  (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    next()
+  }
+)
+
+app.get(
+  `/logout`,
+  (
+    request: Request,
+    response: Response
+  ) => {
+    response.clearCookie('at')
+    response.redirect('/')
+  }
+)
+
+app.get(
+  '*',
+  (
+    request: Request,
+    response: Response
+  ) => {
+    response.sendFile(HTML_FILE)
+  }
+)
+
+app.listen(
+  port,
+  () => console.log(`at work on http://localhost:${port}`)
+)
