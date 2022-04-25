d1c585555c4eceb8e98791beb3082a17c86a3184
paolomococci <paolomococci@gmail.com>
2022-04-25 08:20:23 +0200

    edited file src/graphql/user/login.mutation.ts belonging to project users-ui

---
+++ users-admin/users-ui/src/graphql/user/login.mutation.ts
@@ -0,0 +1,19 @@
+import {
+  gql
+} from '@apollo/client';
+
+export default gql`
+  mutation login(
+    $email: String!,
+    $password: String!
+  ) {
+    login(
+      input: {
+        email: $email,
+        password: $password
+      }
+    ) {
+      token
+    }
+  }
+`
