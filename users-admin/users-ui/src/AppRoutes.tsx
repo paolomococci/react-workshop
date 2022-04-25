7d223e3cb7818fc01e55bee7901584999eabfad9
paolomococci <paolomococci@gmail.com>
2022-04-25 22:26:33 +0200

    edited file src/AppRoutes.tsx belonging to project users-ui

--- users-admin/users-ui/src/AppRoutes.tsx
+++ users-admin/users-ui/src/AppRoutes.tsx
@@ -3,3 +3,21 @@ import {
   Route,
   Switch
 } from 'react-router-dom'
+
+import HomePage from './pages/home'
+import DashboardPage from './pages/dashboard'
+import LoginPage from './pages/login'
+import Error404 from './pages/error404'
+
+const AppRoutes = () => (
+  <Router>
+    <Switch>
+      <Route path="/" component={HomePage} exact />
+      <Route path="/dashboard" component={DashboardPage} exact />
+      <Route path="/login" component={LoginPage} exact />
+      <Route component={Error404} />
+    </Switch>
+  </Router>
+)
+
+export default AppRoutes
